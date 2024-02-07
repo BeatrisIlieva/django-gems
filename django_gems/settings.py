e125ddf65b5901c9e75f8c9e71e2bbe668505306
Beatris Ilieve <beatrisilieve@icloud.com>
2024-02-07 18:32:58 +0200

    Refactored user_account app

--- django_gems/settings.py
+++ django_gems/settings.py
@@ -21,14 +21,14 @@ INSTALLED_APPS = [
     'django_celery_beat',
     'django_countries',
 
-    # 'django_gems.user_account',
-    # 'django_gems.common',
-    # 'django_gems.inventory',
-    # 'django_gems.jewelry',
-    # 'django_gems.order',
-    # 'django_gems.user_profile',
-    # 'django_gems.shopping_cart',
-    # 'django_gems.wishlist',
+    'django_gems.user_account',
+    'django_gems.common',
+    'django_gems.inventory',
+    'django_gems.jewelry',
+    'django_gems.order',
+    'django_gems.user_profile',
+    'django_gems.shopping_cart',
+    'django_gems.wishlist',
 ]
 
 MIDDLEWARE = [
@@ -116,11 +116,11 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# LOGIN_REDIRECT_URL = reverse_lazy('index_page')
-# LOGOUT_REDIRECT_URL = reverse_lazy('login_or_register_user')
-# LOGIN_URL = reverse_lazy('login_or_register_user')
-#
-# AUTH_USER_MODEL = 'accounts.AccountUser'
+LOGIN_REDIRECT_URL = reverse_lazy('index_page')
+LOGOUT_REDIRECT_URL = reverse_lazy('login_or_register_user')
+LOGIN_URL = reverse_lazy('login_or_register_user')
+
+AUTH_USER_MODEL = 'accounts.AccountUser'
 
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'

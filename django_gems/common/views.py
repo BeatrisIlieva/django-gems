b237bdf58ce329126641c1cc46c65fc8d1404e2a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-02-13 21:34:52 +0200

    Refactored views

--- django_gems/common/views.py
+++ django_gems/common/views.py
@@ -97,8 +97,6 @@ class SearchBarView(
 def show_last_viewed(request, pk):
     last_viewed = request.session.get('last_viewed_jewelries', [])
 
-    # request.session._auth_user_id
-
     last_viewed.append(pk)
 
     request.session['last_viewed_jewelries'] = last_viewed

9b570f8ca1e7772d7d25e5cbd4c74cfeb5af9e28
Beatris Ilieve <beatrisilieve@icloud.com>
2024-02-14 14:12:21 +0200

    Refactored tests

--- tests/order/views/test_OrderDetailsView.py
+++ tests/order/views/test_OrderDetailsView.py
@@ -5,9 +5,9 @@ from django.test import Client
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.test import TestCase as TestCase
-from e_commerce_website.inventory.models import Inventory
-from e_commerce_website.order.forms import CardDetailsForm
-from e_commerce_website.jewelry.models import Category, Jewelry
+from django_gems.inventory.models import Inventory
+from django_gems.order.forms import CardDetailsForm
+from django_gems.jewelry.models import Category, Jewelry
 
 
 class AddToShoppingCartViewTests(TestCase):

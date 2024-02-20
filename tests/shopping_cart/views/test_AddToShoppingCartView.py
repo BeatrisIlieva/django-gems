961d7ecb1b908ce33266392dc573014a1c481d00
Beatris Ilieve <beatrisilieve@icloud.com>
2024-02-20 16:53:16 +0200

    Refactored test

--- tests/shopping_cart/views/test_AddToShoppingCartView.py
+++ tests/shopping_cart/views/test_AddToShoppingCartView.py
@@ -1,39 +1,16 @@
-from django.conf import settings
-from django.test import Client
-from django.urls import reverse
 from django.test import TestCase as TestCase
 from django_gems.inventory.models import Inventory
 from django_gems.inventory.utils import remove_quantity_from_inventory
 from django_gems.shopping_cart.models import ShoppingCart
 from django_gems.shopping_cart.views import AddToShoppingCartView
 from django_gems.jewelry.models import Category, Jewelry, Size, JewelrySize
-from django_gems.user_account.models import AccountUser
-from django.test.client import RequestFactory
 
 
 class AddToShoppingCartViewTests(TestCase):
     def setUp(self):
-        self.client = Client()
-
-        self.session = self.client.session
-        self.session.save()
-
-        self.factory = RequestFactory()
-
-        self.client.cookies[settings.SESSION_COOKIE_NAME] \
-            = self.session.session_key
-
         self.category = Category.objects.create(
             title=Category.TitleChoices.NECKLACE
         )
-
-        user_data = {
-            'email': 'beatris@icloud.com',
-            'password': 'securepassword123',
-        }
-
-        self.user = AccountUser.objects.create_user(**user_data)
-        self.client.login(username='beatris@icloud.com', password='securepassword123')
 
         self.jewelry = Jewelry.objects.create(
             title='Test Jewelry',
@@ -109,82 +86,4 @@ class AddToShoppingCartViewTests(TestCase):
         self.assertEqual(
             new_shopping_cart_quantity,
             self.added_quantity_to_shopping_cart
-        )
-
-    def test_add_to_shopping_cart__for_a_second_time__expect_a_quantity_of_two(self):
-        initial_inventory_quantity = \
-            Inventory.objects.get(jewelry=self.jewelry).quantity
-
-        initial_shopping_cart_obj_count = \
-            ShoppingCart.objects.count()
-
-        response = self.client.get(reverse(
-            'add_to_shopping_cart',
-            kwargs={'pk': self.jewelry.pk})
-        )
-
-        new_inventory_quantity = \
-            Inventory.objects.get(jewelry=self.jewelry).quantity
-
-        initial_shopping_cart_quantity = \
-            ShoppingCart.objects.get(jewelry=self.jewelry).quantity
-
-        self.assertEqual(
-            response.status_code,
-            302
-        )
-
-        self.assertEqual(
-            ShoppingCart.objects.count(),
-            initial_shopping_cart_obj_count + 1
-        )
-
-        new_shopping_cart_obj = \
-            ShoppingCart.objects.last()
-
-        new_shopping_cart_obj_count = \
-            ShoppingCart.objects.count()
-
-        self.assertEqual(
-            new_shopping_cart_obj.jewelry,
-            self.jewelry
-        )
-
-        self.assertEqual(
-            new_inventory_quantity,
-            initial_inventory_quantity - self.added_quantity_to_shopping_cart
-        )
-
-        self.assertRedirects(
-            response,
-            reverse('view_shopping_cart')
-        )
-
-        self.client.get(reverse(
-            'add_to_shopping_cart',
-            kwargs={'pk': self.jewelry.pk})
-        )
-
-        updated_shopping_cart_obj_count = \
-            ShoppingCart.objects.count()
-
-        updated_inventory_quantity = \
-            Inventory.objects.get(jewelry=self.jewelry).quantity
-
-        updated_shopping_cart_quantity = \
-            ShoppingCart.objects.get(jewelry=self.jewelry).quantity
-
-        self.assertEqual(
-            updated_inventory_quantity,
-            new_inventory_quantity - self.added_quantity_to_shopping_cart_if_exists
-        )
-
-        self.assertEqual(
-            updated_shopping_cart_quantity,
-            initial_shopping_cart_quantity + self.added_quantity_to_shopping_cart_if_exists
-        )
-
-        self.assertEqual(
-            new_shopping_cart_obj_count,
-            updated_shopping_cart_obj_count
         )
